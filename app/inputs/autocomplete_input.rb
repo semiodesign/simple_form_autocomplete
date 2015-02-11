fa5b7d3bb5324fc0ba28c344af681b743d81e26b
Peter Ragone <pcragone@gmail.com>
2015-02-11 07:26:46 -0500

    Hacked.

--- app/inputs/autocomplete_input.rb
+++ app/inputs/autocomplete_input.rb
@@ -1,13 +1,17 @@
 class AutocompleteInput < SimpleForm::Inputs::CollectionInput
 
-  def input
+  def input wrapper_options
     label, value = detect_collection_methods
-    @reflection = @reflection || object.class.reflections[attribute_name]
-    entity_value = object.send(reflection.name)
+    klass = input_options[:search_class]
+    @reflection = @reflection || object.class.reflections[attribute_name] || klass
+    if reflection.respond_to? :name
+      entity_value = object.send(reflection.name)
+    else
+      entity_value = nil
+    end
 
-    # raise "PCCR: #{entity_value}"
-    case reflection.macro
-    when :belongs_to
+    # case reflection.macro
+    # when :belongs_to
       @builder.hidden_field("#{attribute_name}_id", value: entity_value.try(:id)) +
       template.text_field_tag(
         "autocomplete_for[#{field_name}]",
@@ -19,21 +23,21 @@ class AutocompleteInput < SimpleForm::Inputs::CollectionInput
           :class => input_options[:class],
           :disabled => input_options[:disabled]
       )
-    else
-      template.content_tag(:ul, current_selections.map { |s|
-          selection_tag(s.send(value), s.send(label))
-        }.join('').html_safe,
-        :id => "autocomplete_selections_for_#{object_name}_#{reflection.name}",
-        :class => 'autocomplete_selections') +
-      template.text_field_tag(
-        "autocomplete_multiple_for[#{object_name}_#{reflection.name}]",
-        '',
-        :"data-source" => options[:source],
-        :"data-selections" =>
-          "#autocomplete_selections_for_#{object_name}_#{reflection.name}",
-        :"data-min-chars" => options[:min_chars] || 3,
-        :"data-template" => CGI.escapeHTML(selection_tag("ID", "VALUE")))
-    end
+    # else
+    #   template.content_tag(:ul, current_selections.map { |s|
+    #       selection_tag(s.send(value), s.send(label))
+    #     }.join('').html_safe,
+    #     :id => "autocomplete_selections_for_#{object_name}_#{reflection.name}",
+    #     :class => 'autocomplete_selections') +
+    #   template.text_field_tag(
+    #     "autocomplete_multiple_for[#{object_name}_#{reflection.name}]",
+    #     '',
+    #     :"data-source" => options[:source],
+    #     :"data-selections" =>
+    #       "#autocomplete_selections_for_#{object_name}_#{reflection.name}",
+    #     :"data-min-chars" => options[:min_chars] || 3,
+    #     :"data-template" => CGI.escapeHTML(selection_tag("ID", "VALUE")))
+    # end
   end
 
   def field_name
